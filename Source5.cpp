e07267af91b593977e944e8bea52750ea6ea3e24
jehad-ismail <jehadanwar1@gmail.com>
2025-03-07 14:02:48 +0200

    Add files via upload

---
+++ Source5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	float radius, area;
+	cout << "enter radius of circle =  ";
+	cin >> radius;
+	area = 3.14 * radius * radius;
+	cout << "area of circle is :  " << area;
+
+
+
+
+	return 0;
+}
